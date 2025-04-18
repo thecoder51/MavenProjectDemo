5e93a983bd1f897246f6231648e8cd56633cdc3b
Tarak Shah <tarakshah51@gmail.com>
2025-04-17 22:22:33 -0400

    Dummy push for jenkins

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public void test() {
         System.out.println("A dummy push");
         System.out.println("Another dummy push for jenkins");
+        System.out.println("Tes poll scm on Jenkins");
     }
 }
